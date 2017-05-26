6cb10237ceb19a5c1be81be53402f2bfeb8fbe8c
Ian Hartman <ianhartman@n2-234-7.dhcp.drexel.edu>
2017-05-26 00:38:44 -0400

    added basic idling + job names with basic active job queue

--- NukeWatchdog/hacking.py
+++ NukeWatchdog/hacking.py
@@ -3,14 +3,52 @@ import os
 import subprocess
 import re
 import time
+import threading
+import psutil
 
 
-def worker_main(queue):
+POOL_RUNNING = False
+
+
+class ActivePool(object):
+    def __init__(self):
+        super(ActivePool, self).__init__()
+        self.mgr = multiprocessing.Manager()
+        self.active = self.mgr.list()
+        self.lock = multiprocessing.Lock()
+
+    def makeActive(self, name):
+        with self.lock:
+            self.active.append(name)
+
+    def makeInactive(self, name):
+        with self.lock:
+            self.active.remove(name)
+
+    def __str__(self):
+        with self.lock:
+            return str(self.active)
+
+
+# Simple multiprocessing process work submit function
+def worker_main(queue, pool):
     print 'PID [%s] - Started' % os.getpid()
+
     while True:
         item = queue.get(True)
-        process = subprocess.Popen(item, stdout=subprocess.PIPE)
+
+        # Set the name of the process
+        name = item.pop(0)
+        multiprocessing.current_process().name = name
+
+        pool.makeActive(name)
+
+        custom_env = os.environ.copy()
+        custom_env['TEST'] = '%sPROCESS' % os.getpid()
+        process = subprocess.Popen(item, stdout=subprocess.PIPE, env=custom_env)
         progress(process)
+
+        pool.makeInactive(name)
         # print 'PID [%s] - GET - %s' % (os.getpid(), item)
 
 
@@ -32,15 +70,62 @@ def progress(proc):
             print '[PROGRESS] %s' % line
 
 
+def poolRunning():
+    return POOL_RUNNING
+
+
+# Thread target to check the queue on a constant interval
+# Adds idle sleep process if the queue is empty
+def checkQueue(in_queue, num, pool):
+    # Let the pool grab any tasks if any exist
+    time.sleep(5)
+
+    while True:
+        print 'Running: %s' % str(pool)
+        if in_queue.empty():
+            idle_tasks = []
+
+            # always add # of processes + one to make sure that the queue is full for a moment
+            # Resets check
+            for i in range(num + 1):
+                # give the process a task to basically idle
+                print i
+
+                # Windows has a different sleep command
+                if psutil.WINDOWS:
+                    idle_tasks.append(["IDLE_JOB_%s" % i, 'TIMEOUT', '10'])
+                else:
+                    idle_tasks.append(["IDLE_JOB_%s" % i, 'sleep', '10'])
+
+            for task in idle_tasks:
+                in_queue.put(task)
+        time.sleep(3)
+
+        # Need a better way to set running status
+        if not poolRunning():
+            break
+
+
 if __name__ == '__main__':
+    active_pool = ActivePool()
+
     NUMBER_OF_PROCESSES = 2
     the_queue = multiprocessing.Queue()
-    the_pool = multiprocessing.Pool(NUMBER_OF_PROCESSES, worker_main, (the_queue, ))
+    the_pool = multiprocessing.Pool(NUMBER_OF_PROCESSES, worker_main, (the_queue, active_pool, ))
+    POOL_RUNNING = True
+
+    # Setup thread to constantly check the queue for tasks
+    queue_watcher = threading.Thread(target=checkQueue, name='QueueWatch', args=(the_queue,
+                                                                                 NUMBER_OF_PROCESSES,
+                                                                                 active_pool))
+    queue_watcher.setDaemon(True)
+    queue_watcher.start()
 
     # for i in range(NUMBER_OF_PROCESSES):
     #     multiprocessing.Process(target=worker_main, args=(the_queue,)).start()
 
     tasks = [[
+        "NUKE_JOB_0",
         "/Applications/Nuke10.5v4/Nuke10.5v4.app/Contents/MacOS/Nuke10.5v4",
         "--nukex",
         "-i",
@@ -50,6 +135,7 @@ if __name__ == '__main__':
         "Write1",
         "/Users/ianhartman/Desktop/footage_tracking/_source/track_v001.nk"
     ], [
+        "NUKE_JOB_1",
         "/Applications/Nuke10.5v4/Nuke10.5v4.app/Contents/MacOS/Nuke10.5v4",
         "--nukex",
         "-i",
@@ -58,12 +144,8 @@ if __name__ == '__main__':
         "-X",
         "Write1",
         "/Users/ianhartman/Desktop/footage_tracking/_source/track_v001.nk"
-    ]]
-    for task in tasks:
-        the_queue.put(task)
-
-    time.sleep(2)
-    new_tasks = [[
+    ],[
+        "NUKE_JOB_2",
         "/Applications/Nuke10.5v4/Nuke10.5v4.app/Contents/MacOS/Nuke10.5v4",
         "--nukex",
         "-i",
@@ -72,18 +154,33 @@ if __name__ == '__main__':
         "-X",
         "Write1",
         "/Users/ianhartman/Desktop/footage_tracking/_source/track_v001.nk"
-    ], [
-        "/Applications/Nuke10.5v4/Nuke10.5v4.app/Contents/MacOS/Nuke10.5v4",
-        "--nukex",
-        "-i",
-        "-F",
-        "61-70",
-        "-X",
-        "Write1",
-        "/Users/ianhartman/Desktop/footage_tracking/_source/track_v001.nk"
     ]]
-    for newtask in new_tasks:
-        the_queue.put(newtask)
+    for task in tasks:
+        the_queue.put(task)
+
+    # time.sleep(2)
+    # new_tasks = [[
+    #     "/Applications/Nuke10.5v4/Nuke10.5v4.app/Contents/MacOS/Nuke10.5v4",
+    #     "--nukex",
+    #     "-i",
+    #     "-F",
+    #     "41-50",
+    #     "-X",
+    #     "Write1",
+    #     "/Users/ianhartman/Desktop/footage_tracking/_source/track_v001.nk"
+    # ], [
+    #     "/Applications/Nuke10.5v4/Nuke10.5v4.app/Contents/MacOS/Nuke10.5v4",
+    #     "--nukex",
+    #     "-i",
+    #     "-F",
+    #     "61-70",
+    #     "-X",
+    #     "Write1",
+    #     "/Users/ianhartman/Desktop/footage_tracking/_source/track_v001.nk"
+    # ]]
+    # for newtask in new_tasks:
+    #     the_queue.put(newtask)
+
 
     the_pool.close()
     the_pool.join()
